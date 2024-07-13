cd02f10b0a18e2981f26fdcbb458eece7adb5e60
German Viescas <germanviescas@gmail.com>
2024-07-13 15:51:20 -0300

    Redis repo

--- internal/questionnaire/questionnaire.go
+++ internal/questionnaire/questionnaire.go
@@ -5,7 +5,7 @@ import "github.com/germandv/ama/internal/uid"
 type Questionnaire struct {
 	ID    string `json:"id"`
 	Title string `json:"title"`
-	Host  string `json:"-"`
+	Host  string `json:"host"`
 }
 
 func NewQuestionnaire(title string) Questionnaire {
